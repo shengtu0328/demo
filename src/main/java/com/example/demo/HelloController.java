193f16188b3e7c7f70b52e2d54059aa992c72b81
rqxiao <1007689622@qq.com>
2020-05-13 13:49:16 +0800

    init

---
+++ src/main/java/com/example/demo/HelloController.java
@@ -0,0 +1,21 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author: rqxiao
+ * @Date: 2019-9-5 09:21
+ * @Description:
+ */
+@RestController
+public class HelloController {
+
+
+    @RequestMapping("/hello")
+    public String hello(String a) {
+        return "AAA" + a;
+
+    }
+
+}
